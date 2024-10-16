6d0781a173dd87fb97fa3fa97eb1734923c71b27
dunas26 <jhonjamesillo@gmail.com>
2024-10-16 11:28:31 -0500

    Project clone for cuemby-labs/terraform-static-postgresql

---
+++ variables.tf
@@ -0,0 +1,63 @@
+#
+# Contextual Fields
+#
+
+variable "context" {
+  description = <<-EOF
+Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
+
+Examples:
+```
+context:
+  project:
+    name: string
+    id: string
+  environment:
+    name: string
+    id: string
+  resource:
+    name: string
+    id: string
+```
+EOF
+  type        = map(any)
+  default     = {}
+}
+
+#
+# Connection Fields
+#
+
+variable "hosts" {
+  description = "The host list of the PostgreSQL service."
+  type        = list(string)
+}
+
+variable "hosts_readonly" {
+  description = "The readonly host list of the PostgreSQL service."
+  type        = list(string)
+  default     = []
+}
+
+variable "port" {
+  description = "The port of the PostgreSQL service."
+  type        = number
+  default     = 5432
+}
+
+variable "database" {
+  description = "The name of the PostgreSQL database to access."
+  type        = string
+  default     = ""
+}
+
+variable "username" {
+  description = "The username of the account to access the database."
+  type        = string
+}
+
+variable "password" {
+  description = "The password of the account to access the database."
+  type        = string
+  sensitive   = true
+}
